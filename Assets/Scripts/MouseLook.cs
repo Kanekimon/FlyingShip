81713fac2ac4210a6403ad5f718965f4beb48257
Kanekimon <Kanekimon@web.de>
2021-11-24 12:02:50 +0100

    Initial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class MouseLook : MonoBehaviour
+    {
+
+        public float mouseSensitivity = 100f;
+
+        public Transform playerBody;
+
+        private float xRotation = 0f;
+        private float yRotation = 0f;
+
+        public static MouseLook Instance;
+
+        private bool _isLocked = true;
+
+
+        public bool IsLocked
+        {
+            get { return _isLocked; }
+            set { _isLocked = value; }
+        }
+
+
+        private void Awake()
+        {
+            if (Instance != null)
+                Destroy(this);
+            else
+                Instance = this;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_isLocked)
+            {
+                float mouseX = NormalizeSpeed(Input.GetAxis("Mouse X"));
+                float mouseY = NormalizeSpeed(Input.GetAxis("Mouse Y"));
+
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+                transform.localRotation = Quaternion.Euler(xRotation, 0, 0f);
+                playerBody.Rotate(Vector3.up * mouseX);
+            }
+        }
+
+        private float NormalizeSpeed(float val)
+        {
+            return val * mouseSensitivity * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    public Rigidbody shipBody;
+    public float Speed = 5f;
+    public float GroundDistance = 0.2f;
+    public float DashDistance = 5f;
+    public LayerMask Ground;
+    public float deceleration = 0.5f;
+    public float engineForce = 10f;
+    public float fuel = 100f;
+
+    [SerializeField]
+    private Vector3 _inputs = Vector3.zero;
+    [SerializeField]
+    private Vector3 _moveVector = Vector3.zero;
+    private bool _isGrounded = true;
+    [SerializeField]
+    private Transform _groundChecker;
+    [SerializeField]
+    private Camera cam;
+    private bool _keepHeight = false;
+    private bool _upwardsMovement;
+    [SerializeField]
+    private float _maxAcceleration;
+    private float _fuelUsage = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CalcMaxAcc();
+    }
+
+
+    void CalcMaxAcc()
+    {
+        _maxAcceleration = (Physics.gravity.y * -1) + shipBody.mass + engineForce;
+    }
+
+    private void OnValidate()
+    {
+        CalcMaxAcc();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+
+        _inputs.x = Input.GetAxis("Horizontal");
+        _inputs.z = Input.GetAxis("Vertical");
+
+        _moveVector = Vector3.zero;
+
+        if (_inputs != Vector3.zero)
+        {
+            if (_inputs.z < 0)
+                _moveVector += cam.transform.forward * -1;
+            if (_inputs.z > 0)
+                _moveVector += cam.transform.forward;
+            if (_inputs.x < 0)
+                _moveVector += cam.transform.right * -1;
+            if (_inputs.x > 0)
+                _moveVector += cam.transform.right;
+
+
+            //_moveVector.x += _inputs.x;
+            //_moveVector.z += _inputs.z;
+
+        }
+        else
+        {
+            if (_moveVector.x > 0 || _moveVector.x < 0)
+                _moveVector.x = Mathf.Max(0, _moveVector.x - deceleration);
+            if (_moveVector.z > 0 || _moveVector.z < 0)
+                _moveVector.z = Mathf.Max(0, _moveVector.z - deceleration);
+        }
+
+        _moveVector.y = 0;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            _inputs.y +=  engineForce / ((Physics.gravity.y * -1) * shipBody.mass) ;
+            _inputs.y = Mathf.Clamp(_inputs.y, 0f, _maxAcceleration);
+            fuel -= _fuelUsage * 4;
+            _upwardsMovement = true;
+            _keepHeight = false;
+        }
+        else
+        {
+            _upwardsMovement = false;
+        }
+
+        //if (_testing)
+        //{
+
+        //    if (_lastPos == Vector3.zero)
+        //    {
+        //        _lastPos = this.transform.position;
+        //        _inputs.y = 25f;
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Last Pos: " + _lastPos);
+        //        Debug.Log("Current Pos: " + this.transform.position);
+        //        if (_lastPos.y < this.transform.position.y)
+        //        {
+        //            _inputs.y -= 0.1f;
+        //        }
+        //        _lastPos = this.transform.position;
+        //    }
+
+        //    _upwardsMovement = true;
+        //}
+  
+
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _keepHeight = !_keepHeight;
+        }
+
+        if (!_upwardsMovement)
+        {
+            if (_keepHeight && fuel > 0)
+            {
+                _inputs.y = (shipBody.mass + Physics.gravity.y *-1);
+                fuel -= _fuelUsage;
+            }
+            else
+            {
+                _inputs.y = 0f;
+            }
+        }
+
+        if(_inputs.y > 0)
+            shipBody.AddForce(0, _inputs.y, 0, ForceMode.Force);
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (_moveVector != Vector3.zero)
+            shipBody.MovePosition(shipBody.position + _moveVector * Speed * Time.fixedDeltaTime);
+    }
+}
